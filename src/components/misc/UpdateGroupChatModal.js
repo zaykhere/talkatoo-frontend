a457f6f5c2ffd628bde3ccd9c6464d0ec2daacd1
zain <zainjhere@gmail.com>
2024-07-28 18:33:51 +0500

    Add/remove users group chat functionality

--- src/components/misc/UpdateGroupChatModal.js
+++ src/components/misc/UpdateGroupChatModal.js
@@ -1,8 +1,10 @@
-import { Box, Button, FormControl, IconButton, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, useDisclosure, useToast } from '@chakra-ui/react';
+import { Box, Button, FormControl, IconButton, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Spinner, useDisclosure, useToast } from '@chakra-ui/react';
 import React, { useState } from 'react'
 import { ChatState } from '../../Context/ChatProvider';
 import { FaEye } from "react-icons/fa";
 import UserBadgeItem from '../UserAvatar/UserBadgeItem';
+import API from '../../api/API';
+import UserListItem from '../UserAvatar/UserListItem';
 
 const UpdateGroupChatModal = ({fetchAgain, setFetchAgain}) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -15,11 +17,152 @@ const UpdateGroupChatModal = ({fetchAgain, setFetchAgain}) => {
 
   const { selectedChat, setSelectedChat, user } = ChatState();
 
-  const handleRemove = (userToRemove) => {}
+  const handleRemove = async (user1) => {
+    if (selectedChat.groupAdmin._id !== user._id && user1._id !== user._id) {
+      toast({
+        title: "Only admins can remove someone!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
 
-  const handleRename = () => {}
+    try {
+      setLoading(true);
+      const { data } = await API.put(
+        `/chat/remove-from-group`,
+        {
+          chatId: selectedChat._id,
+          userId: user1._id,
+        }
+      );
 
-  const handleSearch = () => {}
+      user1._id === user._id ? setSelectedChat() : setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+    setGroupChatName("");
+  };
+
+  const handleRename = async () => {
+    if(!groupChatName) return;
+
+    try {
+      setRenameLoading(true);
+
+      const {data} = API.put(`/chat/rename`, {
+        chatId: selectedChat?._id,
+        chatName: groupChatName
+      });
+
+      setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setRenameLoading(false);
+
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setRenameLoading(false);
+    }
+    setGroupChatName("");
+  }
+
+  const handleSearch = async (query) => {
+    setSearch(query);
+    if (!query) {
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const { data } = await API.get(`/user?search=${search}`);
+      console.log(data);
+      setLoading(false);
+      setSearchResult(data);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: "Failed to Load the Search Results",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-left",
+      });
+      setLoading(false);
+    }
+  };
+
+  const handleAddUser = async (userToAdd) => {
+    if (selectedChat.users.find((u) => u._id === userToAdd?._id)) {
+      toast({
+        title: "User Already in group!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    if (selectedChat.groupAdmin._id !== user._id) {
+      toast({
+        title: "Only admins can add someone!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const { data } = await API.put(
+        `/chat/add-to-group`,
+        {
+          chatId: selectedChat._id,
+          userId: userToAdd._id,
+        }
+      );
+
+      setSelectedChat(data);
+      setFetchAgain(!fetchAgain);
+      setLoading(false);
+    } catch (error) {
+      toast({
+        title: "Error Occured!",
+        description: error.response.data.message,
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      setLoading(false);
+    }
+    finally {
+      setGroupChatName("");
+    }
+    
+    
+  }
 
   return (
     <>
@@ -71,6 +214,17 @@ const UpdateGroupChatModal = ({fetchAgain, setFetchAgain}) => {
                 onChange={(e) => handleSearch(e.target.value)}
               />
             </FormControl>
+            {loading ? (
+              <Spinner size="lg" />
+            ) : (
+              searchResult?.map((user) => (
+                <UserListItem
+                  key={user._id}
+                  user={user}
+                  handleFunction={() => handleAddUser(user)}
+                />
+              ))
+            )}
           </ModalBody>
 
           <ModalFooter>
